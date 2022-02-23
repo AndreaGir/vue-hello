d52212d0477416ed9f1da3207da1032bff245f86
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-23 16:15:02 +0100

    ...

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const app = new Vue(
                 message: " Ciao , mi chiamo Andrea e riesco ad utillizare Vue JS",
                 email: "chewbacca1123@gmail.com",
                 img: "scott-estrada-developer.jpg",
-                messagedue: "ora sono un ragzzo felice come quest'uomo qui sopra, grazie boolean :)",
+                messagedue: "ora sono un ragazzo felice come quest'uomo qui sopra, grazie boolean :)",
+                className: "colore",
+                    
+                   
             }
+            
         },
         
 );
