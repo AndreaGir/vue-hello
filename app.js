fa4d493ed001f5980c8ae1bad630c504870a62bd
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-23 15:20:14 +0100

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = new vue (
+        {
+            el: '#divElemento',
+        },
+        data : {
+            message : " Ciao , mi chiamo Andrea e riesco ad utillizare Vue JS"
+        }
+
+
+
+
+);
