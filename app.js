0dd0770ee320a47a440735c661e77b9fa179c10e
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-23 16:00:01 +0100

    2

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const app = new vue (
+const app = new Vue(
         {
             el: '#divElemento',
+            data: {
+                message: " Ciao , mi chiamo Andrea e riesco ad utillizare Vue JS",
+                email: "chewbacca1123@gmail.com",
+                img: "scott-estrada-developer.jpg",
+                messagedue: "ora sono un ragzzo felice come quest'uomo qui sopra, grazie boolean :)",
+            }
         },
-        data : {
-            message : " Ciao , mi chiamo Andrea e riesco ad utillizare Vue JS"
-        }
-
-
-
-
+        
 );
